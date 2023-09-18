50147ccf438bc5553c38d6c9c7227787129a7032
Dallin Osmun <dosmun@gmail.com>
2023-09-18 10:43:58 -0600

    allow markdown notes to be favorited

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -91,14 +91,27 @@ interface FavoriteLinkProps {
   favorite: Favorite
 }
 
+const favClasses = `flex w-72 items-center gap-1 rounded-md border border-black px-4 py-2 transition-all [transition-duration:.3s]`
+
 function FavoriteLink ({ favorite }: FavoriteLinkProps) {
   if (favorite.type === 'list') {
     return (
       <Link
         to={`/list?id=${favorite.id}`}
-        className='flex w-72 items-center gap-1 rounded-md border border-black bg-blue-50 px-4 py-2 transition-all [transition-duration:.3s] hover:bg-blue-100'
+        className={cx(favClasses, 'bg-blue-50 hover:bg-blue-100')}
       >
         <Icons.BookOpen className='h-4 w-4' />
+        <div>{favorite.title}</div>
+      </Link>
+    )
+  }
+  if (favorite.type === 'markdown') {
+    return (
+      <Link
+        to={`/markdown/${favorite.id}`}
+        className={cx(favClasses, 'bg-green-50 hover:bg-green-100')}
+      >
+        <Icons.PencilSquare className='h-4 w-4' />
         <div>{favorite.title}</div>
       </Link>
     )
--- app/routes/markdown_.$id.tsx
+++ app/routes/markdown_.$id.tsx
@@ -8,6 +8,7 @@ import { ClientOnly } from '../components/client-only'
 import * as Icons from '../components/icons'
 import { SimpleMdeReact } from '../components/md-editor.client'
 import * as db from '../utils/db.server'
+import * as settings from '../utils/settings.server'
 
 export function links () {
   return [{ rel: 'stylesheet', href: styles }]
@@ -15,15 +16,29 @@ export function links () {
 
 export async function loader ({ params }: LoaderArgs) {
   const item = await db.getItem(params.id as string)
-  return json({ item })
+  const favorited = settings.getFavorite('markdown', params.id as string)
+  return json({ item, favorited })
 }
 
-export async function action ({ request }: ActionArgs) {
+export async function action ({ request, params }: ActionArgs) {
   const form = await request.formData()
-  const id = form.get('id')
-  const body = form.get('body')
-  if (typeof id !== 'string' || typeof body !== 'string') return
-  await db.updateBody(id, body)
+
+  switch (form.get('_action')) {
+    case 'updateBody': {
+      const id = form.get('id')
+      const body = form.get('body')
+      if (typeof id !== 'string' || typeof body !== 'string') return
+      await db.updateBody(id, body)
+      break
+    }
+    case 'toggleFavorite': {
+      settings.toggleFavorite('markdown', params.id as string)
+      break
+    }
+    default: {
+      throw new Error('unknown action')
+    }
+  }
   return null
 }
 
@@ -39,7 +54,7 @@ const options = {
 }
 
 export default function Index () {
-  const { item } = useLoaderData<typeof loader>()
+  const { item, favorited } = useLoaderData<typeof loader>()
   const fetcher = useFetcher()
 
   return (
@@ -58,17 +73,31 @@ export default function Index () {
         <span>{item.title || 'unnamed'}</span>
         <Link
           to={item.parentId ? `/list?id=${item.parentId}` : '/list'}
-          className='hidden rounded-full bg-blue-100 p-1 group-hover:block'
+          className='rounded-full bg-blue-100 p-1 opacity-0 transition-all group-hover:opacity-100'
         >
           <Icons.ArrowUpLeft className='h-4 w-4 stroke-blue-500' />
         </Link>
+        <button
+          className='group/inner rounded-full p-2 opacity-0 transition-all hover:bg-blue-200 group-hover:opacity-100'
+          onClick={async () => fetcher.submit({ _action: 'toggleFavorite' }, { method: 'post' })}
+        >
+          <Icons.Heart
+            className={cx('h-4 w-4', {
+              'fill-red-500': favorited,
+              'transition-all group-hover/inner:fill-red-300': !favorited
+            })}
+          />
+        </button>
       </h1>
       <ClientOnly>
         {() => (
           <SimpleMdeReact
             value={item.body}
             onChange={value => {
-              fetcher.submit({ id: item.id, body: value }, { method: 'post' })
+              fetcher.submit(
+                { _action: 'updateBody', id: item.id, body: value },
+                { method: 'post' }
+              )
             }}
             options={options}
           />
